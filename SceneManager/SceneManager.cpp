12526f17379023d97a5369a3811567d185333607
Unal Kalkan <server96.unal@gmail.com>
2018-04-09 22:50:15 +0300

    Input handling added for ESCAPE key.

---
+++ SceneManager/SceneManager.cpp
@@ -0,0 +1,6 @@
+
+
+// using namespace SceneManager;
+class SceneManager{
+
+};
--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main(){
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while(!glfwWindowShouldClose(window)){
+        processInput(window);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -42,4 +44,9 @@ int main(){
 }
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
+}
+void processInput(GLFWwindow* window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
 }
